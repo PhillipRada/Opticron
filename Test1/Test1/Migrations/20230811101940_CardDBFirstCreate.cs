642d9f3e806f998b2dc1aae73bd7931ea04ec0c4
Phillip Radasevic <98527875+Phxiil@users.noreply.github.com>
2023-08-11 11:39:57 +0100

    Finaly commit

---
+++ Test1/Test1/Migrations/20230811101940_CardDBFirstCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CardDBFirstCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CardContent",
+                columns: table => new
+                {
+                    CardId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ButtonText = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CardContent");
+        }
+    }
+}
